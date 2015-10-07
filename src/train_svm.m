77cdb4be6e7f3db43a2f7b6d0c6b48d3fe279d2c
Marcello <marcel.buehler@uzh.ch>
2015-10-07 17:39:49 +1100

    added implementation of svm

--- src/train_svm.m
+++ src/train_svm.m
@@ -1,7 +1,7 @@
-function svm = train_svm(training_set)
+function svm = train_svm(X,Y)
 %% Creates and trains a Support Vector Machine using the training set. 
 % The training set should contain two SIFT descriptors as features and a
 % binary output value (1 corresponds to "match" and 0 means "no match")
-svm = 0
+svm = fitcsvm(X,Y,'linear','rbf','Standardize',true,'ClassNames',{'no match','match'});
 
 return
