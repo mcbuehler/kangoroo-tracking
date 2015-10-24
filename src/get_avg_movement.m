e6bd0c1b7cd9519b7985461b9b469a8d3df28dce
Marcello <marcel.buehler@uzh.ch>
2015-10-24 17:11:24 +1100

    starting clean implementation of euclid

--- src/get_avg_movement.m
+++ src/get_avg_movement.m
@@ -1,5 +1,6 @@
 function [x_vec, y_vec] = get_avg_movement(X_o,X_n,Y_o,Y_n)
 
-x_vec = mean(sum(X_n - X_o));
-y_vec = mean(sum(Y_n - Y_o));
+numPoints = length(X_o);
+x_vec = mean(X_n - X_o);
+y_vec = mean(Y_n - Y_o);
 end
