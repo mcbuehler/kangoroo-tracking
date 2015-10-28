4d325f3970b424496871b1999fb2be99f96afe22
Marcello <marcel.buehler@uzh.ch>
2015-10-28 17:56:24 +1100

    started integration SVM

--- src/run_ptb.m
+++ src/run_ptb.m
@@ -9,6 +9,7 @@ conf
 % 4: compute key points for both frames and match them using euclid
 % 5: compute key points for both frames and match them using SVM
 mode = 4;
+mode = 5;
 
 %todo
 %gui
@@ -96,7 +97,8 @@ if mode == 4
         %define array of matching keypoints[xo, xn, yo,yn]
         matches = [];
         
-        %iterate through all keypoints from the last img
+        %iterate through all keypoints from the last img and try to find
+        %new matching key points
         for kpLastID = 1:size(fLast,2)
             
             %get coordinates of keypoint
@@ -105,11 +107,12 @@ if mode == 4
             
             %get rect that defines the current keypoint's neighborhood
             neighbourhoodRect = enlarge_rectangle_pixel(x,y,0,0, boundExpander);
-            %todo check if still in img            
+            neighbourhood = fitToImage(neighbourhoodRect,img);
             
             %find best match within neighbourhood
             bestMatchKeypoint =[];
-            bestMatchEuclid = 100000;
+            bestMatchEuclid = inf;
+            bestMatchSVM = -1;
             
             %find best match in neighbourhood
             %iterate through all keypoints from the current img
@@ -119,11 +122,192 @@ if mode == 4
                     
                     %calculate euclidian distance
                     euclid = norm(double(dLast(:,kpLastID) - dCurrent(:,kpCurrentID)));
-%                     [label,scores] = predict(svm,double(dLast(:,kpLastID) - dCurrent(:,kpCurrentID))')
-%                     input('press enter')
+                    
                     %new best match?
                     if euclid < bestMatchEuclid
                         bestMatchEuclid = euclid;
+                        bestMatchKeypoint = fCurrent(:,kpCurrentID);
+                    end
+                    
+                    
+                end
+            end
+            
+            %check if best match is good enough
+            %add to matches [xo, xn, yo,yn]
+            if numel(bestMatchKeypoint) > 0 && bestMatchEuclid < euclidThreshold
+                % one row per match, format: xOld xNew yOld yNew
+                matches = [matches; fLast(1,kpLastID) bestMatchKeypoint(1) fLast(2,kpLastID) bestMatchKeypoint(2)];
+            end
+        end
+        
+        %clear in case no matches were found
+        x_move = [];
+        y_move =[];
+        
+        if numel(matches )> 0
+            X_n = matches(:,2);
+            X_o = matches(:,1);
+            Y_n = matches(:,4);
+            Y_o = matches(:,3);
+            
+            %get movement vectors
+            x_move = (X_n - X_o);
+            y_move = (Y_n - Y_o);
+            [x_mean,y_mean] = get_avg_movement(X_o,X_n,Y_o,Y_n);
+            
+            %discard non moving key points
+            if discardNonMovingPoints == 1
+                c = 0;
+                for i = size(x_move):-1:1
+                    if abs(x_move(i)) < moveThreshold && abs(y_move(i)) < moveThreshold
+                        x_move(i) = [];
+                        y_move(i) = [];
+                        
+                        %just for correct plotting
+                        X_n(i) = [];
+                        Y_n(i) = [];
+                        
+                        
+                        c=c+1;
+                        
+                    end
+                end
+                
+                fprintf('> non-moving key points discarded %i\n',c);
+            end
+            
+            %discard all movements in the wrong direction
+            if discardWrongMovements == 1
+                if x_mean > 0
+                    x_move(x_move <0) = [];
+                elseif x_mean < 0
+                    x_move(x_move >0) = [];
+                end
+                if y_mean > 0
+                    y_move(y_move <0) = [];
+                elseif y_mean < 0
+                    y_move(y_move >0) = [];
+                end
+            end
+        end
+        %skip if no movement(otherwise errors)
+        if numel(x_move) > 0 && numel(y_move) > 0
+            
+            %get mean movement (after deleting movements in wrong
+            %direction)
+            x_mean = mean(x_move);
+            y_mean = mean(y_move);
+            debug('> %i key points matched \n moving rectangle x %f and y %f\n',[numel(x_move),x_mean,y_mean]);
+            %move objRect
+            x2 = objRect(1)+x_mean;
+            y2 = objRect(2)+y_mean;
+            w2 = objRect(3);
+            h2 = objRect(4);
+            
+            % compute new bounding box for display
+            X = [objRect(1), x2]'; Y = [objRect(2), y2]'; W = [objRect(3), w2]'; H = [objRect(4), h2]';
+            
+            if plotKeypoints == 1
+                plot_tmp(img,X_n,Y_n);
+            end
+            
+            draw(rgb,X,Y,W,H);
+            drawnow
+            
+            counter = counter + 1;
+            if stopEveryXImage > 0 && mod(counter,stopEveryXImage) == 0
+                waitforbuttonpress
+            else
+                pause(0.1);
+            end
+            
+            % save result needed for ptb evaluation
+            result(frameId,:) = [x2, y2, w2, h2];
+            
+            %build new objRect
+            objRect = [x2, y2, w2, h2];
+            
+            %find keypoints of new img that are within the border of the
+            %new objRect
+            %(use array with all kp again, not only those that match with
+            %old kp)
+            fLast = [];
+            dLast = [];
+            for kpCurrentID = 1:size(fCurrent,2)
+                if RectContainsPoint(objRect,fCurrent(1,kpCurrentID), fCurrent(2,kpCurrentID))
+                    %add to fLast and dLast
+                    fLast = [fLast fCurrent(:,kpCurrentID)];
+                    dLast = [dLast dCurrent(:,kpCurrentID)];
+                end
+            end
+        end
+    end
+    
+elseif mode == 5
+    % format: x y w h
+    objRect = load([directory 'init.txt']);
+    
+    %prepare img
+    imageName = fullfile(directory,sprintf('rgb/r-%d-%d.png', frames.imageTimestamp(1), frames.imageFrameID(1)));  
+    rgb = imread(imageName);
+    img = preprocess_image(rgb);
+
+    %get first keypoints
+    [fLast,dLast] = get_dsift_in_bound(img,objRect,m);
+    
+    for frameId = 2:numOfFrames  
+      
+        % load and prepare next frame
+        imageName = fullfile(directory,sprintf('rgb/r-%d-%d.png', frames.imageTimestamp(frameId), frames.imageFrameID(frameId))) ; 
+        rgb = imread(imageName);  
+        fprintf('> processing frame %d\n',frameId)
+        img = preprocess_image(rgb);
+        
+        % we will compute all the key points in the expanded rectangle in
+        % the new frame
+        expRect = enlarge_rectangle_pixel(objRect(1), objRect(2), objRect(3),objRect(4), boundExpander);
+        expRect = fitToImage(expRect,img);
+        [fCurrent,dCurrent] =  get_dsift_in_bound(img,expRect,m);
+        
+        %define array of matching keypoints[xo, xn, yo,yn]
+        matches = [];
+        
+        %iterate through all keypoints from the last img and try to find
+        %new matching key points
+        for kpLastID = 1:size(fLast,2)
+            
+            %get coordinates of keypoint
+            x = fLast(1,kpLastID);
+            y = fLast(2,kpLastID);
+            
+            %get rect that defines the current keypoint's neighborhood
+            neighbourhoodRect = enlarge_rectangle_pixel(x,y,0,0, boundExpander);
+            neighbourhood = fitToImage(neighbourhoodRect,img);           
+            
+            %find best match within neighbourhood
+            bestMatchKeypoint =[];
+            bestMatchEuclid = inf;
+            bestMatchSVM = -1;
+            
+            %find best match in neighbourhood
+            %iterate through all keypoints from the current img
+            for kpCurrentID = 1:size(fCurrent,2)
+                kpCurrentID
+                %if keypoint is in neighbourhood of x and y
+                if (RectContainsPoint(neighbourhoodRect,fCurrent(1,kpCurrentID), fCurrent(2,kpCurrentID)))
+                    
+                    %calculate euclidian distance
+                    euclid = norm(double(dLast(:,kpLastID) - dCurrent(:,kpCurrentID)));
+                    [label,score] = predict(svm,double(dLast(:,kpLastID) - dCurrent(:,kpCurrentID))');
+%                     input('press enter')
+                    %new best match?
+%                     if euclid < bestMatchEuclid
+%                         bestMatchEuclid = euclid;
+%                         bestMatchKeypoint = fCurrent(:,kpCurrentID);
+%                     end
+                    if score(2) > bestMatchSVM
+                        bestMatchSVM = score(2);
                         bestMatchKeypoint = fCurrent(:,kpCurrentID);
                     end
                     
@@ -132,8 +316,8 @@ if mode == 4
             
             %check if best match is good enough
             %add to matches [xo, xn, yo,yn]
-           
-            if numel(bestMatchKeypoint) > 0 && bestMatchEuclid < euclidThreshold
+            if numel(bestMatchKeypoint) > 0 && bestMatchSVM > 0.5 %&& bestMatchEuclid < euclidThreshold
+                % one row per match, format: xOld xNew yOld yNew
                  matches = [matches; fLast(1,kpLastID) bestMatchKeypoint(1) fLast(2,kpLastID) bestMatchKeypoint(2)];
             end
         end
@@ -147,16 +331,11 @@ if mode == 4
                 X_o = matches(:,1);
                 Y_n = matches(:,4);
                 Y_o = matches(:,3);
-
-
-
+                
                 %get movement vectors
                 x_move = (X_n - X_o);
                 y_move = (Y_n - Y_o);
-
-                %get  mean
-                x_mean = mean(x_move);
-                y_mean = mean(y_move);
+                [x_mean,y_mean] = get_avg_movement(X_o,X_n,Y_o,Y_n);
 
                 %discard non moving key points
                 if discardNonMovingPoints == 1
@@ -177,19 +356,15 @@ if mode == 4
                     end
 
                      fprintf('> non-moving key points discarded %i\n',c);
-
                 end
 
                 %discard all movements in the wrong direction
                 if discardWrongMovements == 1
-
-
                     if x_mean > 0
                         x_move(x_move <0) = [];
                     elseif x_mean < 0
                          x_move(x_move >0) = [];
                     end
-
                     if y_mean > 0
                         y_move(y_move <0) = [];
                     elseif y_mean < 0
@@ -204,29 +379,23 @@ if mode == 4
                 %direction)
                 x_mean = mean(x_move);
                 y_mean = mean(y_move);
-
-
-
-
-                   fprintf('> %i key points matched \n moving rectangle x %f and y %f\n',numel(x_move),x_mean,y_mean);
-                    %move objRect
-                    x2 = objRect(1)+x_mean;
-                    y2 = objRect(2)+y_mean;
-                    w2 = objRect(3);
-                    h2 = objRect(4);
+                debug('> %i key points matched \n moving rectangle x %f and y %f\n',[numel(x_move),x_mean,y_mean]);
+                %move objRect
+                x2 = objRect(1)+x_mean;
+                y2 = objRect(2)+y_mean;
+                w2 = objRect(3);
+                h2 = objRect(4);
          
                 % compute new bounding box for display
-                %  [x2,y2,w2,h2] = compute_rectangle(X_n,Y_n);
                 X = [objRect(1), x2]'; Y = [objRect(2), y2]'; W = [objRect(3), w2]'; H = [objRect(4), h2]';
-
-                %display rect
     
                 if plotKeypoints == 1
                     plot_tmp(img,X_n,Y_n);
                 end
-                 draw(rgb,X,Y,W,H);
-                %wait
-                %waitforbuttonpress
+                
+                draw(rgb,X,Y,W,H);
+                drawnow
+                
                 counter = counter + 1;
                 if stopEveryXImage > 0 && mod(counter,stopEveryXImage) == 0
                     waitforbuttonpress
@@ -256,75 +425,72 @@ if mode == 4
             end  
     end 
     
+
 else
     
-    XYZcam = zeros(480,640,4,numOfFrames);
-
-    % XYZcam = zeros(480,640,4,numOfFrames);
 % 
-
-result = zeros(numOfFrames,4);
-% format: x y w h
-bounds = load([directory 'init.txt']);
-
-imageName = fullfile(directory,sprintf('rgb/r-%d-%d.png', frames.imageTimestamp(startFrameId), frames.imageFrameID(startFrameId)));  
-rgb = imread(imageName);
-I_o = preprocess_image(rgb);
-% End code kangoroo-tracking group
-
-for frameId = startFrameId:numOfFrames  
-    imageName = fullfile(directory,sprintf('rgb/r-%d-%d.png', frames.imageTimestamp(frameId), frames.imageFrameID(frameId)));  
-    rgb = imread(imageName);
-    % ------------------------------
-    % Start Code kangoroo-tracking group
-    fprintf('> processing frame %d\n',frameId)
-    I_n = preprocess_image(rgb);
-
-    % get new key points
-    [f1,d1] = get_dsift_in_bound(I_o,bounds,m);
-
-    X_o = f1(1,:);
-    Y_o = f1(2,:);
-    
-    if mode == 1
-        [X_n,Y_n] = align_keypoints_svm(svm,I_n,f1,d1,bounds);
-    elseif mode == 2
-        [X_n,Y_n] = align_keypoints_ubcmatch(I_n,f1,d1,bounds);
-    elseif mode == 3
-        [X_n,Y_n] = align_keypoints_euclid(I_n,f1,d1,bounds);
-    end
-    ;
-    % Only extract aligned points
-    accepted = find(X_n)
-    X_n = X_n(accepted);
-    Y_n = Y_n(accepted);
-    X_o_accepted = X_o(accepted)';
-    Y_o_accepted = Y_o(accepted)';
-    if length(X_n) < 1
-        disp('target lost. Using old coordinates.');
-        X_n = X_o;
-        Y_n = Y_o;
-    end
-    [x_vec,y_vec] = get_avg_movement(X_o_accepted,X_n,Y_o_accepted,Y_n);
-    
-    debug('> moving rectangle x %f and y %f\n',[x_vec,y_vec]);
-    
-    x2 = bounds(1)+x_vec;
-    y2 = bounds(2)+y_vec;
-    w2 = bounds(3);
-    h2 = bounds(4);
-    rect2 = [x2, y2, w2, h2];
-    
-    if getenv('DEBUG') == '1'
-        plot_tmp(I_n,X_o_accepted,Y_o_accepted,X_n,Y_n,bounds,rect2);
-        drawnow
-    end
-    
-    result(frameId,:) = rect2;
-    % reassign variables
-    I_o = I_n;
-    bounds = rect2;
-end
+% result = zeros(numOfFrames,4);
+% % format: x y w h
+% bounds = load([directory 'init.txt']);
+% 
+% imageName = fullfile(directory,sprintf('rgb/r-%d-%d.png', frames.imageTimestamp(startFrameId), frames.imageFrameID(startFrameId)));  
+% rgb = imread(imageName);
+% I_o = preprocess_image(rgb);
+% % End code kangoroo-tracking group
+% 
+% for frameId = startFrameId:numOfFrames  
+%     imageName = fullfile(directory,sprintf('rgb/r-%d-%d.png', frames.imageTimestamp(frameId), frames.imageFrameID(frameId)));  
+%     rgb = imread(imageName);
+%     % ------------------------------
+%     % Start Code kangoroo-tracking group
+%     fprintf('> processing frame %d\n',frameId)
+%     I_n = preprocess_image(rgb);
+% 
+%     % get new key points
+%     [f1,d1] = get_dsift_in_bound(I_o,bounds,m);
+% 
+%     X_o = f1(1,:);
+%     Y_o = f1(2,:);
+%     
+%     if mode == 1
+%         [X_n,Y_n] = align_keypoints_svm(svm,I_n,f1,d1,bounds);
+%     elseif mode == 2
+%         [X_n,Y_n] = align_keypoints_ubcmatch(I_n,f1,d1,bounds);
+%     elseif mode == 3
+%         [X_n,Y_n] = align_keypoints_euclid(I_n,f1,d1,bounds);
+%     end
+%     ;
+%     % Only extract aligned points
+%     accepted = find(X_n)
+%     X_n = X_n(accepted);
+%     Y_n = Y_n(accepted);
+%     X_o_accepted = X_o(accepted)';
+%     Y_o_accepted = Y_o(accepted)';
+%     if length(X_n) < 1
+%         disp('target lost. Using old coordinates.');
+%         X_n = X_o;
+%         Y_n = Y_o;
+%     end
+%     [x_vec,y_vec] = get_avg_movement(X_o_accepted,X_n,Y_o_accepted,Y_n);
+%     
+%     debug('> moving rectangle x %f and y %f\n',[x_vec,y_vec]);
+%     
+%     x2 = bounds(1)+x_vec;
+%     y2 = bounds(2)+y_vec;
+%     w2 = bounds(3);
+%     h2 = bounds(4);
+%     rect2 = [x2, y2, w2, h2];
+%     
+%     if getenv('DEBUG') == '1'
+%         plot_tmp(I_n,X_o_accepted,Y_o_accepted,X_n,Y_n,bounds,rect2);
+%         drawnow
+%     end
+%     
+%     result(frameId,:) = rect2;
+%     % reassign variables
+%     I_o = I_n;
+%     bounds = rect2;
+% end
 end  
 
 disp('> writing data')
