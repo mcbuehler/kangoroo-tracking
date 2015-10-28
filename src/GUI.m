b2625ab6a81532f5b0be33058bf50d7f32c5c614
Marcello <marcel.buehler@uzh.ch>
2015-10-28 22:28:26 +1100

    first cleanup GUI

--- src/GUI.m
+++ src/GUI.m
@@ -62,72 +62,6 @@ else
 end
 % End initialization code - DO NOT EDIT
 
-%Constants
-function value = rectThickness
-value = 3;
-
-function value = rectColor
-value = [255 0 0];
-
-%global variables
-
-%X and Y value of the rectangle the user is currently drawing
-function setFirstX(value)
-global x;
-x = value;
-
-function value = getFirstX
-global x;
-value = x;
-
-function setFirstY(value)
-global y;
-y = value;
-
-function value = getFirstY
-global y;
-value = y;
-
-%currently displayed image
-function setCurrentImg(value)
-global currentImg;
-currentImg = value;
-
-function value = getCurrentImg
-global currentImg;
-value = currentImg;
-
-%rectangles that mark the objects the user has chosen
-function setCurrentRects(value)
-global currentRects;
-currentRects = value;
-
-function value = getCurrentRects
-global currentRects;
-value = currentRects;
-
-%Preview rectangle; shown when user is drawing a rectangle
-function setPreviewRect(value)
-global previewRect;
-previewRect = value;
-
-function value = getPreviewRect
-global previewRect;
-value = previewRect;
-
-%write folderpath to config
-function writeImageFolderPath(value)
-fileID = fopen('conf.txt', 'wt');
-fprintf(fileID,'%',value);
-
-%read folderpath from config
-function value = readImageFolderPath
-conf = fileread('conf.txt');
-if size(conf) > 0
-    value = conf(1);
-else
-    value = '';
-end
 
 % --- Executes just before GUI is made visible.
 function GUI_OpeningFcn(hObject, eventdata, handles, varargin)
@@ -188,187 +122,6 @@ stop = handles.checkStop.Value;
 plotLevel = handles.popDisplay.Value;
 
 run_ptb_function(videoname,matching, stop, plotLevel);
-
-
-%redraws the current image and rectangles
-function Redraw
-%todo
-
-%if ~(getCurrentImg == '')
-
-%add rectangles to image
-locImg = getCurrentImg;
-currentRects = getCurrentRects;
-if size(currentRects(),1) > 0
-    for i = 1:size(currentRects,1)
-        locImg = insertShape(locImg,'Rectangle', currentRects(i,:), 'Color', rectColor, 'LineWidth', rectThickness);
-    end
-end
-
-%draw preview rect
-tempRect = getPreviewRect;
-if size(tempRect,1) > 0
-    locImg = insertShape(locImg,'Rectangle', tempRect(1,:), 'Color', rectColor, 'LineWidth', rectThickness);
-end
-
-%draw image
-imageHandle = imshow(locImg);
-
-%install eventhandler for btndown
-imageHandle.ButtonDownFcn = @ImageBtnDown;
-
-%end
-
-
-%shows image and installs eventhandlers for it
-function SetImage(path)%, image)
-
-%read image file
-%axes(image); why is this done in the excample?
-imageObj = imread(path);
-
-%set current image
-setCurrentImg(imageObj)
-
-%draw
-
-
-%adds a rectangle
-function AddRect(posX, posY, width, height)
-newM = [getCurrentRects; [posX, posY, width, height]];
-setCurrentRects(newM);
-
-
-
-%deletes all rectangles
-function ClearRects()
-setCurrentRects([]);
-
-%
-% %returns rectangle as [x y width height) from any given to points
-% function value = CreateRect(x1,y1,x2,y2)
-% if x2 > x1
-%     leftX = x1;
-%     rightX = x2;
-% else
-%     leftX = x2;
-%     rightX = x1;
-% end
-%
-% if y2 < y1
-%     upperY = y2;
-%     lowerY = y1;
-% else
-%     upperY = y1;
-%     lowerY = y2;
-% end
-%
-% value = [leftX upperY (rightX - leftX) (lowerY - upperY)];
-
-%displays first image of imagefolder ore video file
-function ShowFirstImage
-
-%getFiles
-files = dir(readImageFolderPath);
-if size(files,1) > 0
-    SetImage(files(1,1));
-end
-Redraw;
-
-%TODO
-
-%executed when Mousebutton is released anywhere(there is no ButtonUpFcn for
-%the Image)
-function ImageBtnUp(objecthandle, eventdata)
-
-%get UI handles
-handles = guidata(objecthandle);
-
-%Get get cursor location
-cursorLocation = handles.img.CurrentPoint;
-x = round(cursorLocation(1,1));
-y =  round(cursorLocation(1,2));
-
-%delete preview rectangle
-setPreviewRect([]);
-
-%write cursor location
-%todo delete
-txtSelection = handles.txtSelection;
-locationStr = strcat( num2str(x) , ', ' ,num2str(y));
-txtSelection.String = strcat( txtSelection.String, '---', locationStr);
-
-%create and add rectangle to list
-newRect = compute_rectangle([x;getFirstX], [y; getFirstY]);
-AddRect(newRect(1),newRect(2),newRect(3),newRect(4));
-Redraw;
-
-%remove eventhandler for btnUp and mouse move
-fig = gcf;
-fig.WindowButtonUpFcn = '';
-fig.WindowButtonMotionFcn = '';
-
-
-
-%executed when Mousebutton is pressed on the image
-function ImageBtnDown(objecthandle, eventdata)
-
-%get UI handles
-handles = guidata(objecthandle);
-
-%Get get cursor location
-cursorLocation = handles.img.CurrentPoint;
-x = round(cursorLocation(1,1));
-y =  round(cursorLocation(1,2));
-
-%write location to global variables
-setFirstX(x);
-setFirstY(y);
-
-%write cursor location
-%todo delete
-txtSelection = handles.txtSelection;
-txtSelection.String = strcat( num2str(x) , ', ' ,num2str(y));
-
-%create preview rectangle
-setPreviewRect([x y 0 0]);
-
-%install eventhandler for btnUp and mouse moving
-fig = gcf;
-fig.WindowButtonUpFcn = @ImageBtnUp;
-fig.WindowButtonMotionFcn = @MouseMoving;
-
-function MouseMoving(objecthandle, eventdata)
-
-previewRect = getPreviewRect;
-%if user is drawing a rectangle
-if size(previewRect,1) > 0
-    
-    %get UI handles
-    handles = guidata(objecthandle);
-    
-    %Get cursor location
-    cursorLocation = handles.img.CurrentPoint;
-    x = round(cursorLocation(1,1));
-    y =  round(cursorLocation(1,2));
-    
-    %Update preview rect
-    newPreviewRect = CreateRect([x; getFirstX],[y getFirstY]);
-    setPreviewRect(newPreviewRect);
-    
-    %redraw
-    Redraw;
-end
-
-% --- Outputs from this function are returned to the command line.
-function varargout = GUI_OutputFcn(hObject, eventdata, handles)
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Get default command line output from handles structure
-varargout{1} = handles.output;
 
 
 % --- Executes on button press in btnSelect.
@@ -510,4 +263,13 @@ function stopBtn_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 global TERMINATE
 TERMINATE = 1;
+% --- Outputs from this function are returned to the command line.
+function varargout = GUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
 
