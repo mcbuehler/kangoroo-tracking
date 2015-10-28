d9866c7a1cda02ecd3f1792829698329ba93291f
Marcello <marcel.buehler@uzh.ch>
2015-10-28 23:33:00 +1100

    removed old files

--- src/compare_images.m
+++
@@ -1,7 +0,0 @@
-I1 = imread('D:\Programs\Git\kangoroo-tracking\datasets\zcup_move_1\rgb\r-3467165-69.png');
-I2 = imread('D:\Programs\Git\kangoroo-tracking\datasets\zcup_move_1\rgb\r-3533842-70.png');
-
-subplot(1,2,1);
-imshow(I1);
-subplot(1,2,2);
-imshow(I2);
